d336720045db4da34d394d40262ee87dd50ac590
Devineni Sai Dheeraj <34763932+Dheeru555@users.noreply.github.com>
2018-08-09 09:33:11 -0500

    Update app_test.py

--- tests/app_test.py
+++ tests/app_test.py
@@ -18,13 +18,13 @@ import json
 
  
 
-// def test_ial_alert_insert(url): 
-//     url_formatted = '{0}{1}'.format(url, 'ial_alert') 
-//     payload = get_alert_payload() 
-//     headers = {'content-type': 'application/json'} 
+# def test_ial_alert_insert(url): 
+#     url_formatted = '{0}{1}'.format(url, 'ial_alert') 
+#     payload = get_alert_payload() 
+#     headers = {'content-type': 'application/json'} 
  
 
-//     r = requests.post(url=url_formatted, 
-//                       data=json.dumps(payload), 
-//                       headers=headers) 
-//     assert 201 == r.status_code, r.text 
+#     r = requests.post(url=url_formatted, 
+#                       data=json.dumps(payload), 
+#                       headers=headers) 
+#     assert 201 == r.status_code, r.text 
