7fe0da48ec2935f6a36d96b838922f1abf9e5c5a
Devineni Sai Dheeraj <34763932+Dheeru555@users.noreply.github.com>
2018-08-09 17:53:38 -0500

    Update test_app.py

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -3,15 +3,15 @@ import pytest
 import requests
 import json
 
-def test_get_emp_details(url):
-    url_formatted = '{0}'.format(url)
+def test_get_emp_details():
+    url_formatted = '{0}'.format('http://opsitcdkr01:5067/')
     r = requests.get(url_formatted)
     assert 200 == r.status_code, r.text
      
 
 
-def test_emp_age_details(url):
-    url_formatted = '{0}'.format(url)
+def test_emp_age_details():
+    url_formatted = '{0}'.format('http://opsitcdkr01:5067/')
     r = requests.get(url_formatted)
     data = r.json()
     assert 25 == data['EMP_Details'][0]['age']
@@ -20,8 +20,8 @@ def test_emp_age_details(url):
 
 
 
-def test_emp_post_details(url):
-    url_formatted = '{0}{1}'.format(url, 'emp/check')
+def test_emp_post_details():
+    url_formatted = '{0}{1}'.format('http://opsitcdkr01:5067/', 'emp/check')
     payload = get_alert_payload()
     headers = {'content-type': 'application/json'}
 
