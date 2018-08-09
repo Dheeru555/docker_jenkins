c51f82914490bd06715637f5f32ed054ba43f4cd
Devineni Sai Dheeraj <34763932+Dheeru555@users.noreply.github.com>
2018-08-09 16:15:50 -0500

    Update conftest.py

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,11 +2,11 @@ import pytest
 
 
 def pytest_addoption(parser): 
-    parser.addoption("--urlreq", 
+    parser.addoption("--urlreqs", 
                      default='http://opsitcdkr01:5067/', 
                      help="Specify URL where the APIs are hosted") 
 
 
 @pytest.fixture 
 def url(request): 
-     return request.config.getoption('--urlreq')
+     return request.config.getoption('--urlreqs')
