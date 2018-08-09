744bd2bc6325ab19442bc6dd33d4cc243c2339b2
Devineni Sai Dheeraj <34763932+Dheeru555@users.noreply.github.com>
2018-08-09 14:36:40 -0500

    Update app_test.py

--- tests/app_test.py
+++ tests/app_test.py
@@ -11,20 +11,32 @@ import json
  
 
  def test_get_emp_details(url): 
-     url_formatted = '{0}{1}'.format(url, 'check1/') 
+     url_formatted = '{0}{1}'.format(url, '/check1') 
      r = requests.get(url_formatted) 
      assert 200 == r.status_code, r.text 
+     
  
+ def test_emp_age_details(url): 
+     url_formatted = '{0}{1}'.format(url, '/check1') 
+     r = requests.get(url_formatted)
+     data = r.json()
+     assert 25 == data['EMP_Details'][0]['age']
 
  
 
-# def test_ial_alert_insert(url): 
-#     url_formatted = '{0}{1}'.format(url, 'ial_alert') 
-#     payload = get_alert_payload() 
-#     headers = {'content-type': 'application/json'} 
+ def test_emp_post_details(url): 
+     url_formatted = '{0}{1}'.format(url, '/check1') 
+     payload = get_alert_payload() 
+     headers = {'content-type': 'application/json'} 
  
 
-#     r = requests.post(url=url_formatted, 
-#                       data=json.dumps(payload), 
-#                       headers=headers) 
-#     assert 201 == r.status_code, r.text 
+     r = requests.post(url=url_formatted, 
+                       data=json.dumps(payload), 
+                       headers=headers) 
+     assert 201 == r.status_code, r.text
+   
+def get_alert_payload(): 
+    return { 
+        "name":"Sai Dheeraj Devineni","company":"TCS","age":23
+    }
+  
