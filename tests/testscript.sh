80eaa565e33c8395d79d5f9f7e41edaa91214d1e
Devineni Sai Dheeraj <34763932+Dheeru555@users.noreply.github.com>
2018-08-13 12:36:09 -0500

    Update testscript.sh

--- tests/testscript.sh
+++ tests/testscript.sh
@@ -10,32 +10,15 @@ set -x
  } 
 
  # build and run the composed services 
- TAG=$1 docker-compose -f docker/docker-compose.yml -p ci build && TAG=$1 docker-compose -f docker/docker-compose.yml -p ci up -d 
+ TAG=1 docker-compose -f docker/docker-compose.yml -p ci build && TAG=1 docker-compose -f docker/docker-compose.yml -p ci up -d 
  if [ $? -ne 0 ] ; then 
    printf "Docker Compose Failed\n" 
-   exit -1 
+   exit -1
+ else
+   printf " Tests Passed\n"
  fi 
- 
-
- # wait for the test service to complete and grab the exit code 
- TEST_EXIT_CODE=`docker wait ci_random_test_1` 
- 
-
- # output the logs for the test (for clarity) 
- docker logs ci_random_test_1 
- 
-
- # inspect the output of the test and display respective message 
- if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then 
-   printf "Tests Failed - Exit Code: $TEST_EXIT_CODE\n" 
- else 
-   printf "Tests Passed\n" 
- fi 
- 
 
  # call the cleanup fuction 
  cleanup 
  
-
- # exit the script with the same code as the test service code 
- exit $TEST_EXIT_CODE 
+ 
